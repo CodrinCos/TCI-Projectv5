12cb06396a482ba33f90883eae31b6ff2bd54487
Tudor <tudor_cioata@yahoo.com>
2018-12-15 16:55:07 +0100

    added 1 test

--- WebCrawler/src/test/java/WebCrawlerTest.java
+++ WebCrawler/src/test/java/WebCrawlerTest.java
@@ -1,3 +1,24 @@
+import WebCrawler.WebCrawler.MoviesNode;
+import WebCrawler.WebCrawler.BooksNode;
+import WebCrawler.WebCrawler.MusicNode;
+import WebCrawler.WebCrawler.Node;
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
 public class WebCrawlerTest {
+
+    @Test
+    public void nodeParentCheck(){
+        //arrange
+        Node baseNode = new Node(new Node(),"link");
+        //act
+        Node parent = baseNode.getParent();
+        //assert
+        Assert.assertNotNull(parent);
+    }
+
+
 
 }
